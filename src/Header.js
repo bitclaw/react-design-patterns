e37d0fbf9fdb6126b2acc8c023d7e4965fb8cd3e
Daniel C <bitclaw@gmail.com>
2017-08-16 07:24:02 -0500

    Add header component

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="header">
+                <div className="fa fa-more"></div>
+                <span className="title">Timeline</span>
+                <input
+                    type="text"
+                    className="searchInput"
+                    placeholder="Search ..." />
+                <div className="fa fa-search searchIcon"></div>
+            </div>
+        )
+    }
+}
