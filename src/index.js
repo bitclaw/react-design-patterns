b6ccc3c1a66757a80483e150726abc90a8973c1b
Daniel C <bitclaw@gmail.com>
2017-08-16 08:27:00 -0500

    Import jQuery correctly and remove logo.svg

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import 'jquery/src/jquery';
+import '../node_modules/bootstrap-sass/assets/javascripts/bootstrap.min';
+import '../node_modules/bootstrap-sass/assets/stylesheets/_bootstrap.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
