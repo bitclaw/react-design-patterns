8063f8154224459767bb7168ba59104ef9aef0a7
Daniel C <bitclaw@gmail.com>
2017-08-17 06:56:38 -0500

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
-import { Header } from './Header';
-import { Content } from './Content';
 import './App.css';
 
 class App extends Component {
     render() {
         return (
-            <div className="notificationsFrame">
-                <div className="panel">
-                    <Header />
-                    <Content />
-                </div>
+            <div>
+                TEST
             </div>
         )
     }
